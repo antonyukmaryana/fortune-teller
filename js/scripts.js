4954f90031413d2880b9a85d8ac9b6569c98eae9
Maryana Antonyuk and Svitlana Filatova <maryana@>
2019-04-02 16:45:00 -0700

    Add script

--- js/scripts.js
+++ js/scripts.js
@@ -5,14 +5,32 @@ $(document).ready(function() {
 
   $("form#fortune_teller").submit(function(event){
     event.preventDefault();
-    $("input:checkbox[name=fortune_teller]:checked").each(function(){
-    var person_check = $(this).val()
-    $('#person').append(person_check + "<br>");
+    var userResponses = [];
+    $("input:checkbox[name='person']:checked").each(function(){
+    var person_info = $(this).val();
+    userResponses.push(person_info);
     });
+    $("input:checkbox[name='age']:checked").each(function(){
+    var age_info = $(this).val();
+    userResponses.push(age_info);
+    });
+    $("input:checkbox[name='marital_status']:checked").each(function(){
+    var maritalStatus_info = $(this).val();
+    userResponses.push(maritalStatus_info);
+    });
+    $("input:checkbox[name='children']:checked").each(function(){
+    var children_info = $(this).val();
+    userResponses.push(children_info);
+    });
+    $("input:checkbox[name='parapsychology']:checked").each(function(){
+    var parapsychology_info = $(this).val();
+    userResponses.push(parapsychology_info);
+    });
+    $("input:checkbox[name='money']:checked").each(function(){
+    var money_info = $(this).val();
+    userResponses.push(money_info);
+    });
+console.log(userResponses)
+});
 
-    // if (fortune_tellerMode === "") {
-        // $('#answer1').fadeIn();
-      // }
-
-    })
-})
+});
